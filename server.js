0daab191277bf4bf15417084efd73fe68de6cae6
sagarsarkar043 <sagarsarkar043@gmail.com>
2016-10-06 07:19:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ res.send(counter.toString());
 
 
 });
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '/article-one.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '/index.html'));
 });
 //article one is not working or not displaying because it is not created in the ui folder and  does not have html extension both of which are needed  for doing the complete application
 app.get('/inspi-articles', function (req, res) {

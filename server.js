1e91cb6e62237bd007c01b74470b91228de0b936
sagarsarkar043 <sagarsarkar043@gmail.com>
2016-10-31 14:10:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '/index.html'));
 });
 //article one is not working or not displaying because it is not created in the ui folder and  does not have html extension both of which are needed  for doing the complete application
-app.get('/comment', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '/comment.html'));
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '/login.html'));
 });
 
 
